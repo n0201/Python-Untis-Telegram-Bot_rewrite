1e3d6bd9ff1d75951eb0c08d22e9f76109de76a1
n0201 <lolwiiu123@gmail.com>
2026-01-16 07:34:13 +0100

    fix a small issue

--- main.py
+++ main.py
@@ -32,11 +32,11 @@ def restricted(func):
     return wrapped
 
 def save_object(obj, filename):
-    with open(filename, 'wb') as output:
+    with open(f"data/{filename}", 'wb') as output:
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
 def load_object(filename):
-    with open(filename, 'rb') as input:
+    with open(f"data/{filename}", 'rb') as input:
         return pickle.load(input)
 
 @restricted
